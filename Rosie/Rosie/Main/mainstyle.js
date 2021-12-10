a238dc60ac2addee26a4b6a0e2199be6c60abf8c
lydtn19411 <93891933+lydtn19411@users.noreply.github.com>
2021-12-10 20:01:14 +0700

    fix-cart-homepage-book

---
+++ Rosie/Rosie/Main/mainstyle.js
@@ -0,0 +1,8 @@
+﻿function changePagination(type, element) {
+    let pages = document.getElementsByClassName('page-item');
+    for (i = 0; i < pages.length; i++) {
+        pages[i].style.background = "#ffffff";
+    }
+    element.style.background = "#8B3A62";
+
+}
