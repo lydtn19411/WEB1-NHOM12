4abed0c0baf01b32e5cf82e9e036e381e568df37
lydtn19411 <93891933+lydtn19411@users.noreply.github.com>
2021-12-14 08:27:00 +0700

    add_chitietspsach

---
+++ Rosie/Rosie/Main/java/mainstyle.js
@@ -0,0 +1,29 @@
+﻿function changePagination(type, element) {
+    let pages = document.getElementsByClassName('page-item');
+    for (i = 0; i < pages.length; i++) {
+        pages[i].style.background = "#ffffff";
+    }
+    element.style.background = "#8B3A62";
+
+}
+
+var ProductImg = document.getElementById("ProductImg");
+var SmallImg = document.getElementsByClassName("small_img");
+SmallImg[0].onclick = function () {
+    ProductImg.src = SmallImg[0].src;
+}
+SmallImg[1].onclick = function () {
+    ProductImg.src = SmallImg[1].src;
+}
+SmallImg[2].onclick = function () {
+    ProductImg.src = SmallImg[2].src;
+}
+SmallImg[3].onclick = function () {
+    ProductImg.src = SmallImg[3].src;
+}
+SmallImg[4].onclick = function () {
+    ProductImg.src = SmallImg[4].src;
+}
+
+
+
--- Rosie/Rosie/Main/mainstyle.js
+++
@@ -1,8 +0,0 @@
-﻿function changePagination(type, element) {
-    let pages = document.getElementsByClassName('page-item');
-    for (i = 0; i < pages.length; i++) {
-        pages[i].style.background = "#ffffff";
-    }
-    element.style.background = "#8B3A62";
-
-}
